e77b296d55a2356a9a34ce26bc326435f1ced3d0
Pranav Balagerimath <pranav.balagerimath@colorado.edu>
2025-03-16 16:38:03 -0600

    Add init.sql

---
+++ init.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Users (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password_hash VARCHAR(256) NOT NULL,
+    full_name VARCHAR(100),
+    bio TEXT,
+    profile_picture_url VARCHAR(255),
+    location VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
